000f4a64dcaa2e2f814146b304510113eb4bbe90
Paul Blay <paulmblay@gmail.com>
2020-08-27 16:14:28 +0100

    basic Jira connection code

---
+++ jiraMetrics.py
@@ -0,0 +1,11 @@
+from jira import JIRA
+import yaml
+
+class JiraMetrics:
+    def __init__(self):
+        self.login_dict = yaml.safe_load(open('login.yml'))
+        self.jira_server = self.login_dict.get('jira_url')
+        self.my_username = self.login_dict.get('username')
+        self.api_key = self.login_dict.get('api_key')
+    def connect(self):
+        self.jira = JIRA(server=self.jira_server, basic_auth=(self.my_username,self.api_key))
---
+++ test_jira.py
@@ -0,0 +1,8 @@
+from jiraMetrics import *
+
+def test_object_creation():
+    jirametrics = JiraMetrics()
+
+def test_connection():
+    jirametrics = JiraMetrics()
+    jirametrics.connect()
