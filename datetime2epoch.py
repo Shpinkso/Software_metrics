0071fa7ca3ce620660e88a35ee2b72f1ce860c62
Paul Blay <paulmblay@gmail.com>
2020-08-28 21:59:54 +0100

    Credit to https://gist.github.com/squioc/3078803

--- datetime2epoch.py
+++ datetime2epoch.py
@@ -1,6 +1,8 @@
 from datetime import datetime
 import calendar
 
+# credit to https://gist.github.com/squioc/3078803
+
 class Datetime2Epoch:
     def d2e(self, dt):
         return calendar.timegm(datetime.strptime(dt, "%Y-%m-%dT%H:%M:%S.%f").timetuple())
