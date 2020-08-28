6a286f63c1161e86a853858b1955647d9e01c58a
Paul Blay <paulmblay@gmail.com>
2020-08-28 16:00:18 +0100

    basic gitlab login code, updating some shared data from the login file

--- jiraMetrics.py
+++ jiraMetrics.py
@@ -5,7 +5,7 @@ class JiraMetrics:
     def __init__(self):
         self.login_dict = yaml.safe_load(open('login.yml'))
         self.jira_server = self.login_dict.get('jira_url')
-        self.my_username = self.login_dict.get('username')
-        self.api_key = self.login_dict.get('api_key')
+        self.my_username = self.login_dict.get('jira_username')
+        self.api_key = self.login_dict.get('jira_api_key')
     def connect(self):
         self.jira = JIRA(server=self.jira_server, basic_auth=(self.my_username,self.api_key))
