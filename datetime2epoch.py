10c7dbf3da140c9082fc9a936b29bd108197daca
Paul Blay <paulmblay@gmail.com>
2020-08-29 21:23:35 +0100

    fixed what I think was a timezone bug

--- datetime2epoch.py
+++ datetime2epoch.py
@@ -1,10 +1,24 @@
 from datetime import datetime
 import calendar
+import time
 
-# credit to https://gist.github.com/squioc/3078803
+''' 
+Partial credit to https://gist.github.com/squioc/3078803 although there seems to be
+a bug related to timezones. Converting an ISO 8601 time string to epoch and back again
+caused an hour to be added on somewhere along the line, so my unit tests failed.
+Given that I want to use the same timezone in both directions, I glued together some
+other code to keep everything in GM time. Original code from the website was:
+
+def epoch_to_iso8601(timestamp):
+    return datetime.fromtimestamp(timestamp).isoformat()
+
+def iso8601_to_epoch(datestring):
+    return calendar.timegm(datetime.strptime(datestring, "%Y-%m-%dT%H:%M:%S.%f").timetuple())
+'''
 
 class Datetime2Epoch:
     def d2e(self, dt):
         return calendar.timegm(datetime.strptime(dt, "%Y-%m-%dT%H:%M:%S.%f").timetuple())
     def e2d(self, e):
-        return datetime.fromtimestamp(e).isoformat();
+        mytime = time.gmtime(e)
+        return time.strftime('%Y-%m-%dT%H:%M:%S', mytime)
