4391dfb54a9a4e715b2b69d9c33dfa54e1c2201c
Paul Blay <paulmblay@gmail.com>
2020-09-01 15:56:21 +0100

    remove self from login_dict and added (as yet untested) commits_since function

--- gitlabMetrics.py
+++ gitlabMetrics.py
@@ -4,17 +4,18 @@ from datetime import datetime
 
 class GitlabMetrics:
     def __init__(self):
-        self.login_dict = yaml.safe_load(open('login.yml'))
-        self.gl_server = self.login_dict.get('gitlab_url')
-        self.api_key = self.login_dict.get('gitlab_api_key')
-        self.project_id = self.login_dict.get('gitlab_project_id')
+        login_dict = yaml.safe_load(open('login.yml'))
+        self.gl_server = login_dict.get('gitlab_url')
+        self.api_key = login_dict.get('gitlab_api_key')
+        self.project_id = login_dict.get('gitlab_project_id')
+        self.commits_tbl = login_dict.get('git_commits_table')
     def connect(self):
         self.gl = gitlab.Gitlab(self.gl_server, private_token=self.api_key)
     def get_project(self):
         self.project = self.gl.projects.get(self.project_id)
-    def get_project_commits_since(self, date: datetime):
-        commits = self.project.commits.list(since=date)
-        for commit in commits:
-            print(commit)
+    def get_project_commits_since(self, time: datetime):
+        self.commits = self.project.commits.list(since=time)
     def get_date_of_last_commit_in_database(self) -> datetime:
-        return datetime.today()
+        print("moo")
+    def add_commits_to_database(self):
+        print("add")
