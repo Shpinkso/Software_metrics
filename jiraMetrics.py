83529f74e455081df5a8d03f167909edca868285
Paul Blay <paulmblay@gmail.com>
2020-09-01 14:03:41 +0100

    no need to make login dict a object variable

--- jiraMetrics.py
+++ jiraMetrics.py
@@ -3,9 +3,9 @@ import yaml
 
 class JiraMetrics:
     def __init__(self):
-        self.login_dict = yaml.safe_load(open('login.yml'))
-        self.jira_server = self.login_dict.get('jira_url')
-        self.my_username = self.login_dict.get('jira_username')
-        self.api_key = self.login_dict.get('jira_api_key')
+        login_dict = yaml.safe_load(open('login.yml'))
+        self.jira_server = login_dict.get('jira_url')
+        self.my_username = login_dict.get('jira_username')
+        self.api_key = login_dict.get('jira_api_key')
     def connect(self):
         self.jira = JIRA(server=self.jira_server, basic_auth=(self.my_username,self.api_key))
