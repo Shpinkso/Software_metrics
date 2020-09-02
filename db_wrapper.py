2b563054be5772e11c6984c208a51370fcb4f590
Paul Blay <paulmblay@gmail.com>
2020-09-02 16:18:30 +0100

    added insert function and generalised function for finding largest value

--- db_wrapper.py
+++ db_wrapper.py
@@ -11,8 +11,19 @@ class DBInterface:
     def connect(self):
         self.mydb = pymysql.connect(self.url, self.user, self.pw, self.db)
         self.cursor = self.mydb.cursor()
-    def get_most_recent_record_date(self, date, table):
-        self.cursor.execute("select MAX({}) from {};".format(date,table))
+    def set_table(self,table):
+        self.table = table
+    def get_max_value(self, max_of):
+        self.cursor.execute("select MAX({}) from {};".format(max_of,self.table))
         return self.cursor.fetchone()
-    def insert(self):
-        print("not implemented")
+    def insert(self, schema_tup):
+        self.cursor.execute("INSERT INTO {} VALUES {}".format(self.table, schema_tup))
+        try:
+            self.cursor.execute("")
+            self.mydb.commit
+            result = True
+        except:
+            self.mydb.rollback()
+            result = False
+        return result # need to learn about exceptions in python.. this is a bit C programmery, probs
+
