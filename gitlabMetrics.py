fbce6ea0985b98379525dd883122374cfc0813c2
Paul Blay <paulmblay@gmail.com>
2020-09-02 16:20:03 +0100

    include testmode, connect db wrapper and implement new functions

--- gitlabMetrics.py
+++ gitlabMetrics.py
@@ -1,21 +1,30 @@
 import gitlab
 import yaml
 from datetime import datetime
+from db_wrapper import *
 
 class GitlabMetrics:
-    def __init__(self):
-        login_dict = yaml.safe_load(open('login.yml'))
+    def __init__(self, testmode):
+        login_dict = yaml.safe_load(open('{}login.yml'.format(testmode)))
         self.gl_server = login_dict.get('gitlab_url')
         self.api_key = login_dict.get('gitlab_api_key')
-        self.project_id = login_dict.get('gitlab_project_id')
         self.commits_tbl = login_dict.get('git_commits_table')
+        self.db_connector = DBInterface(testmode);
     def connect(self):
         self.gl = gitlab.Gitlab(self.gl_server, private_token=self.api_key)
+        self.db_connector.connect()
+        self.db_connector.set_table(self.commits_tbl)
+    def load_project(self, project_id):
+        self.project = self.gl.projects.get(project_id)
     def get_project(self):
-        self.project = self.gl.projects.get(self.project_id)
-    def get_project_commits_since(self, time: datetime):
+        return self.project
+    def load_project_commits_since(self, time: datetime):
         self.commits = self.project.commits.list(since=time)
     def get_date_of_last_commit_in_database(self) -> datetime:
-        print("moo")
+        date_tup = self.db_connector.get_max_value()
+        return date_tup[0]
     def add_commits_to_database(self):
-        print("add")
+        for commit in self.commits:
+            schema_tup = (commit.id, commit.committer_name, commit.committed_date)
+            self.db_connector.insert(schema_tup)
+        
