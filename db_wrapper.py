83d830abc73c556888c214a99555b9b5e7195ec9
Paul Blay <paulmblay@gmail.com>
2020-09-03 21:20:20 +0100

    Ensure table exists in set_table, change return value for raising exception

--- db_wrapper.py
+++ db_wrapper.py
@@ -12,18 +12,22 @@ class DBInterface:
         self.mydb = pymysql.connect(self.url, self.user, self.pw, self.db)
         self.cursor = self.mydb.cursor()
     def set_table(self,table):
-        self.table = table
+        self.cursor.execute('USE {}'.format(self.db))
+        self.cursor.execute('SHOW TABLES')
+        table_tup = (table,)
+        tables_in_db = [c for c in self.cursor]
+        if table_tup in tables_in_db:
+            self.table = table
+        else:
+            raise ValueError("table {} does not exist in {}".format(table,self.db))
     def get_max_value(self, max_of):
         self.cursor.execute("select MAX({}) from {};".format(max_of,self.table))
         return self.cursor.fetchone()
     def insert(self, schema_tup):
-        self.cursor.execute("INSERT INTO {} VALUES {}".format(self.table, schema_tup))
         try:
-            self.cursor.execute("")
-            self.mydb.commit
-            result = True
+            self.cursor.execute("INSERT INTO {} VALUES {}".format(self.table, schema_tup))
+            self.mydb.commit()
         except:
             self.mydb.rollback()
-            result = False
-        return result # need to learn about exceptions in python.. this is a bit C programmery, probs
+            raise
 
