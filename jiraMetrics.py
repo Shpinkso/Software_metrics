bb2d257a9e957d0e5c50310ddac556816a70325a
Paul Blay <paulmblay@gmail.com>
2020-09-07 16:06:43 +0100

    flesh out jira functionality

--- jiraMetrics.py
+++ jiraMetrics.py
@@ -1,11 +1,58 @@
 from jira import JIRA
 import yaml
+from datetime import datetime
+from db_wrapper import *
+from datetime2epoch import *
 
 class JiraMetrics:
-    def __init__(self):
-        login_dict = yaml.safe_load(open('login.yml'))
+    def __init__(self, testmode):
+        login_dict = yaml.safe_load(open('{}login.yml'.format(testmode)))
         self.jira_server = login_dict.get('jira_url')
         self.my_username = login_dict.get('jira_username')
         self.api_key = login_dict.get('jira_api_key')
+        self.tasks_tbl = login_dict.get('jira_task_table')
+        self.db_connector = DBInterface(testmode);
     def connect(self):
         self.jira = JIRA(server=self.jira_server, basic_auth=(self.my_username,self.api_key))
+        self.db_connector.connect()
+        self.db_connector.set_table(self.tasks_tbl)
+    def load_project(self, project_key):
+        self.project = None
+        projects = self.jira.projects()
+        for project in projects:
+            if project.key == project_key:
+                self.project = project_key
+                return
+        raise ValueError("project key {} does not exist".format(project_key))
+    def load_done_project_issues_since(self, time: datetime):
+        converter = Datetime2Epoch()
+        datetime_obj = converter.clean_datetime(time)
+        jira_dt_str = "{}-{}-{} {}:{}".format(datetime_obj.year,
+                                              datetime_obj.month,
+                                              datetime_obj.day,
+                                              datetime_obj.hour,
+                                              datetime_obj.minute)
+        search_str = 'project = "{}" AND status = done AND updated > "{}"'.format(
+                self.project,
+                jira_dt_str)
+        self.issues = self.jira.search_issues(search_str)
+    def get_loaded_issues(self):
+        return self.issues
+    def get_date_of_last_done_issue_in_database(self):
+        date_tup = self.db_connector.get_max_value('UPDATED_DT')
+        return date_tup[0]
+    def __get_lead_time(self, created, updated):
+        converter = Datetime2Epoch()
+        dt_updated = converter(updated)
+        dt_created = converter(created)
+        delta = dt_updated - dt_created
+        return delta
+    def add_issues_to_database(self):
+        for issue in self.issues:
+            leadtime = self.__get_lead_time(issue.fields.created,issue.fields.updated)
+            print("add {}, {}, {}, {}".format(issue.key,
+                                              issue.fields.created,
+                                              issue.fields.updated,
+                                              leadtime))
+            schema_tup = (issue.key, issue.fields.created,issue.fields.updated,leadtime)
+            self.db_connector.insert(schema_tup)
--- test_jira.py
+++ test_jira.py
@@ -1,8 +1,92 @@
 from jiraMetrics import *
+from datetime import datetime, timedelta
+import pytest
+
+testmode = 'test_'
+jira_project_key = 'MM'
+
+@pytest.fixture(scope="function")
+def make_test_database():
+    #setup
+    login_dict = yaml.safe_load(open('test_login.yml'))
+    table_name = login_dict.get('jira_task_table')
+    url = login_dict.get('mysql_url')
+    user = login_dict.get('mysql_user')
+    pw = login_dict.get('mysql_password')
+    db = login_dict.get('mysql_db')
+    test_db = pymysql.connect(url, user, pw, db)
+    cursor = test_db.cursor()
+    sql = """CREATE TABLE {} (
+             TASK_ID VARCHAR(20) NOT NULL,
+             CREATED DATETIME NOT NULL,
+             UPDATED DATETIME NOT NULL,
+             DELTA_HOURS INT(20) NOT NULL)""".format(table_name)
+    try:
+        cursor.execute(sql)
+    except:
+        assert False
+    sql = """INSERT INTO {} VALUES (
+             'MM-1',
+             '2020-09-07 11:28:55',
+             '2020-09-07 11:28:56',
+             '0')""".format(table_name)
+    try:
+        cursor.execute(sql)
+        test_db.commit()
+    except:
+        test_db.rollback()
+        assert False
+    sql = """INSERT INTO {} VALUES (
+             'MM-2',
+             '2020-09-07 11:45:27',
+             '2020-09-08 11:45:27',
+             '24')""".format(table_name)
+    try:
+        cursor.execute(sql)
+        test_db.commit()
+    except:
+        test_db.rollback()
+        assert False
+    sql = """INSERT INTO {} VALUES (
+             'MM-3',
+             '2020-08-01 11:45:27',
+             '2020-08-05 11:45:27',
+             '96')""".format(table_name)
+    try:
+        cursor.execute(sql)
+        test_db.commit()
+    except:
+        test_db.rollback()
+        assert False
+
+    yield test_db
+    #teardown
+    cursor.execute("DROP TABLE IF EXISTS {}".format(table_name))
 
 def test_object_creation():
-    jirametrics = JiraMetrics()
+    jirametrics = JiraMetrics(testmode)
 
-def test_connection():
-    jirametrics = JiraMetrics()
+def test_connection(make_test_database):
+    jirametrics = JiraMetrics(testmode)
     jirametrics.connect()
+
+def test_load_valid_project_does_not_raise_exception(make_test_database):
+    jirametrics = JiraMetrics(testmode)
+    jirametrics.connect()
+    try:
+        jirametrics.load_project(jira_project_key)
+    except Exception as e:
+        assert False, "{}".format(e)
+
+def test_load_done_project_issues_doesnt_get_prior_issues(make_test_database):
+    jirametrics = JiraMetrics(testmode)
+    jirametrics.connect()
+    try:
+        jirametrics.load_project(jira_project_key)
+    except Exception as e:
+        assert False, "{}".format(e)
+    jirametrics.load_done_project_issues_since("2020-09-02T00:00:00.000")
+    older_issues = ['MM-1','MM-2','MM-3','MM-4','MM-5']
+    issues = jirametrics.get_loaded_issues()
+    for issue in issues:
+        assert issue.key not in older_issues
