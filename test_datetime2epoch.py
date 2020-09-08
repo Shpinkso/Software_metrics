34cb8e44449768724c6333843b9aec12ca4cd574
Paul Blay <paulmblay@gmail.com>
2020-09-08 16:31:05 +0100

    add some tests to check timezones and datetime input

--- test_datetime2epoch.py
+++ test_datetime2epoch.py
@@ -1,5 +1,6 @@
 from datetime2epoch import *
 from datetime import datetime
+import dateutil.tz
 
 def test_object_creation():
     converter = Datetime2Epoch()
@@ -24,3 +25,30 @@ def test_epoch_to_date_at_time_zero():
     dt = converter.e2d(epoch)
     assert dt == correct_datetime #"1970-01-01T00:00:00"
 
+def test_string_to_datetime_deals_with_datetime():
+    converter = Datetime2Epoch()
+    test_dt = datetime(2010,11,20,14,23,0)
+    dt = converter.string_to_datetime(test_dt)
+    assert dt == test_dt
+
+def test_string_to_datetime_deals_with_no_timezone():
+    converter = Datetime2Epoch()
+    time_str = "2012-07-09T22:27:50.272517"
+    check = datetime(2012,7,9,22,27,50,272517)
+    dt = converter.string_to_datetime(time_str)
+    assert dt == check
+
+def test_string_to_datetime_deals_with_plus_timezone():
+    converter = Datetime2Epoch()
+    time_str = "2020-09-08T10:01:44.000-04:00"
+    check = datetime(2020,9,8,10,1,44, tzinfo=dateutil.tz.tzoffset(None, -14400))
+    dt = converter.string_to_datetime(time_str)
+    assert dt == check
+
+
+def test_string_to_datetime_deals_with_minus_timezone():
+    converter = Datetime2Epoch()
+    time_str = "2020-09-08T10:01:44.000+04:00"
+    check = datetime(2020,9,8,10,1,44, tzinfo=dateutil.tz.tzoffset(None, 14400))
+    dt = converter.string_to_datetime(time_str)
+    assert dt == check
