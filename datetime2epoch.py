4b8e4762c531fe9773f7f460c0b7cc0e8136aa58
Paul Blay <paulmblay@gmail.com>
2020-09-08 16:29:17 +0100

    made string_to_datetime both input-parameter-type lenient and fix a bug with conversion of -ve timezones

--- datetime2epoch.py
+++ datetime2epoch.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 import calendar
 import time
-
+from dateutil import parser
 ''' 
 Partial credit to https://gist.github.com/squioc/3078803 although there seems to be
 a bug related to timezones. Converting an ISO 8601 time string to epoch and back again
@@ -18,8 +18,12 @@ def iso8601_to_epoch(datestring):
 
 class Datetime2Epoch:
     def string_to_datetime(self, dt):
-        removed_zone = dt.split('+',1)[0] # We're removing the timezone to simplify. We only care about days for now
-        result = datetime.strptime(removed_zone, '%Y-%m-%dT%H:%M:%S.%f')
+        if type(dt) is datetime:
+            result = dt
+        else:
+            result = parser.parse(dt)
+#        removed_zone = dt.split('+',1)[0] # We're removing the timezone to simplify. We only care about days for now
+#        result = datetime.strptime(removed_zone, '%Y-%m-%dT%H:%M:%S.%f')
         print("converted {} {} to {} {}".format(type(dt),dt, type(result),result))
         return result
     def d2e(self, dt: datetime) -> int:
